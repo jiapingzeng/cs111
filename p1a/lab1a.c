09f568640a7af5c1fd647bc3bfc0abf4afed69eb
Jiaping Zeng <jzeng@outlook.com>
2020-01-13 21:49:47 -0800

    started part 3 of lab1a

--- p1a/lab1a.c
+++ p1a/lab1a.c
@@ -12,23 +12,49 @@ struct termios oldconfig;
 void set_input_mode();
 void reset_input_mode();
 
-int main() {
+int main(int argc, char **argv) {
+
+    if (argc == 2) {
+        if (strcmp(argv[1], "--shell") == 0) {
+            __pid_t pid = fork();
+            int inpipe[2];
+            int outpipe[2];
+
+            if (pid == 0) {
+                printf("child");
+                pipe(inpipe);
+                printf("inpipe: %d, %d", inpipe[0], inpipe[1]);
+                pipe(outpipe);
+                printf("outpipe: %d, %d", outpipe[0], outpipe[1]);
+                return 0;
+                execl("/bin/bash", "bash", NULL);
+                fprintf(stderr, "Failed to execute shell");
+            } else {
+                printf("parent");
+                return 0;
+            }
+        } else {
+            fprintf(stderr, "Invalid argument");
+            exit(1);
+        }
+    } else if (argc > 2) {
+        fprintf(stderr, "Invalid number of arguments");
+        exit(1);
+    }
+
     char ch;
     set_input_mode();
     while (1) {
         read(STDIN_FILENO, &ch, 1);
         if (ch == '\004')
             break;
+        else if (ch == '\r' || ch == '\n')
+            write(STDOUT_FILENO, "\r\n", 2);
         else
             write(STDOUT_FILENO, &ch, 1);
     }
-    //reset_input_mode();
-    
-    return 0;
-}
 
-void reset_input_mode() {
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldconfig);
+    return 0;
 }
 
 void set_input_mode() {
@@ -40,4 +66,8 @@ void set_input_mode() {
     newconfig.c_oflag = 0;
     newconfig.c_lflag = 0;
     tcsetattr(STDIN_FILENO, TCSANOW, &newconfig);
+}
+
+void reset_input_mode() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldconfig);
 }
