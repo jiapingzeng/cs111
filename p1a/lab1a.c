6387f5e547bef6a7d8ab3e8154a3e7f19a9da128
Jiaping Zeng <jzeng@outlook.com>
2020-01-25 14:18:14 -0800

    mostly works including error handling

--- p1a/lab1a.c
+++ p1a/lab1a.c
@@ -1,3 +1,7 @@
+// NAME: Jiaping Zeng
+// EMAIL: jiapingzeng@ucla.edu
+// ID: 905363270
+
 #include <termios.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -6,114 +10,247 @@
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <signal.h>
 
+static int shell_flag;
+static int debug_flag;
 struct termios oldconfig;
+int fwd[2], bwd[2];
+int exitcode;
 
-void run_terminal();
+void parse_options(int argc, char **argv);
+void set_input_mode();
 void reset_input_mode();
+void read_data(int fd, __pid_t pid);
+void if_error(int error, char *message);
+void handle_shell_exit(int i, __pid_t pid);
+void handle_sigpipe();
 
-int main(/*int argc, char **argv*/) {
+int main(int argc, char **argv)
+{
+    parse_options(argc, argv);
+    set_input_mode();
+    if (shell_flag)
+    {
+        exitcode = pipe(fwd);
+        if_error(exitcode, "Forward pipe failed");
+        exitcode = pipe(bwd);
+        if_error(exitcode, "Backward pipe failed");
+        signal(SIGPIPE, handle_sigpipe);
+        __pid_t pid = fork();
+        if_error(pid, "Fork failed");
+        if (pid > 0)
+        {
+            // parent
+            int retval, i;
+            struct pollfd pfds[2];
 
-    __pid_t pid = fork();
-    int pipe1[2]; // read from terminal, write to shell
-    int pipe2[2]; // read from shell, write to terminal
+            exitcode = close(fwd[0]);
+            if_error(exitcode, "Unable to close read end of forward pipe");
+            exitcode = close(bwd[1]);
+            if_error(exitcode, "Unable to close write end of backward pipe");
 
-    // create pipes and exit on failure
-    if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
-        fprintf(stderr, "Pipe failed\n");
-        exit(1);
-    }
+            pfds[0].fd = STDIN_FILENO;
+            pfds[1].fd = bwd[0];
+            pfds[0].events = POLLIN | POLLHUP | POLLERR;
+            pfds[1].events = POLLIN | POLLHUP | POLLERR;
 
-    if (pid > 0) {
-        // parent (terminal)
-        char *input = "pwd\0";
-        char buffer[256];
-        close(pipe1[0]);
-        write(pipe1[1], input, strlen(buffer));
-        close(pipe1[1]);
-
-        wait(NULL);
-        close(pipe2[1]);
-        read(pipe2[0], buffer, 256);
-        printf("%s\n", buffer);
-        close(pipe2[0]);
-        //run_terminal();
-    } else if (pid == 0) {
-        // child (exec bash)
-        close(pipe1[1]);
-        dup2(pipe1[0], STDIN_FILENO);
-        close(pipe1[0]);
-
-        close(pipe2[0]);
-        dup2(pipe2[1], STDOUT_FILENO);
-        close(pipe2[1]);
-        execl("/bin/bash", "bash", NULL);
-    } else {
-        // error
-        fprintf(stderr, "Unable to fork");
-    }
-
-    /**
-    if (argc == 2) {
-        if (strcmp(argv[1], "--shell") == 0) {
-            __pid_t pid = fork();
-            int inpipe[2];
-            int outpipe[2];
-
-            if (pid == 0) {
-                printf("child");
-                pipe(inpipe);
-                printf("inpipe: %d, %d", inpipe[0], inpipe[1]);
-                pipe(outpipe);
-                printf("outpipe: %d, %d", outpipe[0], outpipe[1]);
-                return 0;
-                execl("/bin/bash", "bash", NULL);
-                fprintf(stderr, "Failed to execute shell");
-            } else {
-                printf("parent");
-                return 0;
+            while (1)
+            {
+                retval = poll(pfds, (nfds_t)2, 0);
+                if_error(retval, "Poll failed");
+                for (i = 0; i < 2; i++)
+                {
+                    if (pfds[i].revents & POLLIN)
+                        read_data(pfds[i].fd, pid);
+                    if (pfds[i].revents & (POLLHUP | POLLERR))
+                        handle_shell_exit(i, pid);
+                }
             }
-        } else {
-            fprintf(stderr, "Invalid argument");
-            exit(1);
         }
-    } else if (argc > 2) {
-        fprintf(stderr, "Invalid number of arguments");
-        exit(1);
+        else if (pid == 0)
+        {
+            // child
+            exitcode = close(fwd[1]);
+            if_error(exitcode, "Unable to close write end of forward pipe");
+            exitcode = dup2(fwd[0], STDIN_FILENO);
+            if_error(exitcode, "Unable to dup forward read to stdin");
+            exitcode = dup2(bwd[1], STDOUT_FILENO);
+            if_error(exitcode, "Unable to dup backward write to stdout");
+            exitcode = dup2(bwd[1], STDERR_FILENO);
+            if_error(exitcode, "Unable to dup backward write to stderr");
+            exitcode = execl("/bin/bash", "bash", NULL);
+            if_error(exitcode, "Execl failed");
+        }
     }
-
-    run_terminal();
-    **/
-    return 0;
+    else
+    {
+        char buffer[8], ch;
+        int bytes_read, i;
+        // read keypresses
+        while (1)
+        {
+            bytes_read = read(STDIN_FILENO, &buffer, 8);
+            if_error(bytes_read, "Unable to read from stdin");
+            for (i = 0; i < bytes_read; i++)
+            {
+                ch = buffer[i];
+                switch (ch)
+                {
+                case '\003':
+                case '\004':
+                    exit(0);
+                    break;
+                case '\r':
+                case '\n':
+                    exitcode = write(STDOUT_FILENO, "\r\n", 2);
+                    if_error(exitcode, "Unable to write CRLF to stdout");
+                    break;
+                default:
+                    exitcode = write(STDOUT_FILENO, &ch, 1);
+                    if_error(exitcode, "Unable to write buffer to stdout");
+                    break;
+                }
+            }
+        }
+    }
+    exit(0);
 }
 
-void run_terminal() {
-    char ch;
-    struct termios newconfig;
+void parse_options(int argc, char **argv)
+{
+    int c, option_index;
+    static struct option long_options[] = {
+        {"shell", no_argument, &shell_flag, 's'},
+        {"debug", no_argument, &debug_flag, 'd'},
+        {0, 0, 0, 0}};
+    while (1)
+    {
+        c = getopt_long(argc, argv, "sd", long_options, &option_index);
+        if (c == -1)
+            break;
+        switch (c)
+        {
+        case 0:
+            break;
+        case '?':
+            fprintf(stderr, "Unable to retrieve option\n");
+            exit(1);
+        default:
+            fprintf(stderr, "Unknown option: %d\n", c);
+            exit(1);
+        }
+    }
+}
 
+void set_input_mode()
+{
+    struct termios newconfig;
     // set input mode
-    tcgetattr(STDIN_FILENO, &oldconfig);
+    exitcode = tcgetattr(STDIN_FILENO, &oldconfig);
+    if_error(exitcode, "Unable to get terminal parameters");
     atexit(reset_input_mode);
-    tcgetattr(STDIN_FILENO, &newconfig);
+    exitcode = tcgetattr(STDIN_FILENO, &newconfig);
+    if_error(exitcode, "Unable to get terminal parameters");
     newconfig.c_iflag = ISTRIP;
     newconfig.c_oflag = 0;
     newconfig.c_lflag = 0;
-    tcsetattr(STDIN_FILENO, TCSANOW, &newconfig);
+    exitcode = tcsetattr(STDIN_FILENO, TCSANOW, &newconfig);
+    if_error(exitcode, "Unable to set terminal parameters");
+}
 
-    printf("terminal running");
+void reset_input_mode()
+{
+    exitcode = tcsetattr(STDIN_FILENO, TCSANOW, &oldconfig);
+    if_error(exitcode, "Unable to set terminal parameters");
+}
 
-    // read keypresses
-    while (1) {
-        read(STDIN_FILENO, &ch, 1);
-        if (ch == '\004')
-            break;
-        else if (ch == '\r' || ch == '\n')
-            write(STDOUT_FILENO, "\r\n", 2);
-        else
-            write(STDOUT_FILENO, &ch, 1);
+void read_data(int fd, __pid_t pid)
+{
+    int i;
+    int bytes_read;
+    char buffer[256];
+    bytes_read = read(fd, buffer, 256);
+    if_error(bytes_read, "Unable to read buffer");
+    if (fd == STDIN_FILENO)
+    {
+        // terminal
+        for (i = 0; i < bytes_read; i++)
+        {
+            switch (buffer[i])
+            {
+            case '\003':
+                exitcode = kill(pid, SIGINT);
+                if_error(exitcode, "Unable to kill child process");
+                break;
+            case '\004':
+                exitcode = close(fwd[1]);
+                if_error(exitcode, "Unable to close write end of forward pipe");
+                break;
+            case '\r':
+            case '\n':
+                exitcode = write(STDOUT_FILENO, "\r\n", 2);
+                if_error(exitcode, "Unable to write CRLF to stdout");
+                exitcode = write(fwd[1], "\n", 1);
+                if_error(exitcode, "Unable to write LF to forward pipe");
+                break;
+            default:
+                exitcode = write(STDOUT_FILENO, &buffer[i], 1);
+                if_error(exitcode, "Unable to write buffer to stdout");
+                exitcode = write(fwd[1], &buffer[i], 1);
+                if_error(exitcode, "Unable to write buffer to forward pipe");
+                break;
+            }
+        }
+    }
+    else
+    {
+        // shell
+        for (i = 0; i < bytes_read; i++)
+        {
+            switch (buffer[i])
+            {
+            case '\r':
+            case '\n':
+                exitcode = write(STDOUT_FILENO, "\r\n", 2);
+                if_error(exitcode, "Unable to write CRLF to stdout");
+                break;
+            default:
+                exitcode = write(STDOUT_FILENO, &buffer[i], 1);
+                if_error(exitcode, "Unable to write buffer to stdout");
+                break;
+            }
+        }
     }
 }
 
-void reset_input_mode() {
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldconfig);
+void handle_shell_exit(int i, __pid_t pid)
+{
+    int status;
+    if (i == 0)
+    {
+        exitcode = kill(pid, SIGINT);
+        if_error(exitcode, "Unable to kill child process");
+    }
+    exitcode = waitpid(pid, &status, 0);
+    if_error(exitcode, "Waitpid failed");
+    fprintf(stderr, "SHELL EXIT SIGNAL=%d, STATUS=%d\r\n", WTERMSIG(status), WEXITSTATUS(status));
+    close(bwd[0]);
+    exit(0);
+}
+
+void handle_sigpipe()
+{
+    fprintf(stderr, "Received SIGPIPE\n");
+    exit(0);
+}
+
+void if_error(int error, char *message)
+{
+    if (error < 0)
+    {
+        fprintf(stderr, "%s\n", message);
+        exit(1);
+    }
 }
