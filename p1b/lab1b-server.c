d30bfbf66d83f59912e7fb94f19eb1334969ade5
Jiaping Zeng <jzeng@outlook.com>
2020-01-25 16:27:22 -0800

    started lab1b

---
+++ p1b/lab1b-server.c
@@ -0,0 +1,86 @@
+#include <unistd.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#define PORT 3000
+
+static int port;
+int exitcode;
+
+void parse_options(int argc, char **argv);
+void if_error(int error, char *message);
+
+int main(int argc, char **argv) {
+
+    parse_options(argc, argv);
+
+    int sfd, newsfd, bytes_read;
+    struct sockaddr_in addr;
+    unsigned int len;
+    char buffer[1024];
+
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if_error(sfd, "Unable to open socket");
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+    len = sizeof(addr);
+
+    exitcode = bind(sfd, (struct sockaddr *)&addr, sizeof(addr));
+    if_error(exitcode, "Bind failed");
+
+    exitcode = listen(sfd, 5);
+    if_error(exitcode, "Listen failed");
+
+    newsfd = accept(sfd, (struct sockaddr *)&addr, sizeof(addr));
+    if_error(exitcode, "Accept failed");
+
+    bytes_read = read(newsfd, buffer, 1024);
+    if_error(bytes_read, "Unable to read from new socket");
+
+    printf("%s\n", buffer);
+
+    char *test = "Server test";
+    send(newsfd, test, strlen(test), 0);
+
+    printf("Server sent message successfully\n");
+
+    exit(0);
+}
+
+void parse_options(int argc, char **argv) {
+    int c;
+    static struct option long_options[] = {
+        {"port", required_argument, NULL, 'p'},
+        {0, 0, 0, 0}
+    };
+
+    while ((c = getopt_long(argc, argv, "p", long_options, NULL)) != -1) {
+        switch (c) {
+            case 0:
+                // set flag
+                break;
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case '?':
+                fprintf(stderr, "Unable to retrieve option\n");
+                exit(1);
+            default:
+                fprintf(stderr, "Unknown option: %d\n", c);
+                exit(1);
+        }
+    }
+}
+
+void if_error(int error, char *message) {
+    if (error < 0) {
+        fprintf(stderr, "%s\n", message);
+        exit(1);
+    }
+}
