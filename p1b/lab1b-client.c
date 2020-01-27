b6134c28da6727ee4969c8c66431ad4f051db907
Jiaping Zeng <jzeng@outlook.com>
2020-01-26 23:55:15 -0800

    rough draft of client and server

--- p1b/lab1b-client.c
+++ p1b/lab1b-client.c
@@ -7,28 +7,37 @@
 #include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
-#define PORT 3000
+#include <termios.h>
+#include <poll.h>
 
-static int port;
 int exitcode;
+static int compress_flag;
+struct termios oldconfig;
+int sfd;
 
-void parse_options(int argc, char **argv);
+void read_data(int fd);
+void set_input_mode();
+void reset_input_mode();
+void parse_options(int argc, char **argv, int *port, char **log);
 void if_error(int error, char *message);
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    int port;
+    char *log;
 
-    parse_options(argc, argv);
+    parse_options(argc, argv, &port, &log);
+    set_input_mode();
 
-    int sfd, bytes_read;
+    int i;
     unsigned int len;
     struct sockaddr_in addr;
-    char buffer[1024];
-    
+
     sfd = socket(AF_INET, SOCK_STREAM, 0);
     if_error(sfd, "Unable to open socket");
 
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
+    addr.sin_port = htons(port);
     len = sizeof(addr);
 
     exitcode = inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
@@ -37,44 +46,134 @@ int main(int argc, char **argv) {
     exitcode = connect(sfd, (struct sockaddr *)&addr, (socklen_t)len);
     if_error(exitcode, "Connection failed");
 
-    char *test = "Client test";
-    send(sfd, test, strlen(test), 0);
+    struct pollfd pfds[2];
+    pfds[0].fd = STDIN_FILENO;
+    pfds[1].fd = sfd;
+    pfds[0].events = POLLIN | POLLHUP | POLLERR;
+    pfds[1].events = POLLIN | POLLHUP | POLLERR;
 
-    printf("Client sent test\n");
-
-    bytes_read = read(sfd, buffer, 1024);
-    printf("%s\n", buffer);
-
+    while (1)
+    {
+        exitcode = poll(pfds, 2, 0);
+        if_error(exitcode, "Poll failed");
+        for (i = 0; i < 2; i++)
+        {
+            if (pfds[i].revents & POLLIN)
+            {
+                read_data(pfds[i].fd);
+            }
+            if (pfds[i].revents & (POLLHUP | POLLERR))
+            {
+                fprintf(stderr, "Client poll error\n");
+                exit(1);
+            }
+        }
+    }
     exit(0);
 }
 
-void parse_options(int argc, char **argv) {
-    int c;
-    static struct option long_options[] = {
-        {"port", required_argument, NULL, 'p'},
-        {0, 0, 0, 0}
-    };
-
-    while ((c = getopt_long(argc, argv, "p", long_options, NULL)) != -1) {
-        switch (c) {
-            case 0:
-                // set flag
+void read_data(int fd)
+{
+    int i, bytes_read;
+    char buffer[256];
+    bytes_read = read(fd, buffer, 256);
+    if_error(bytes_read, "Unable to read buffer");
+    if (fd == STDIN_FILENO)
+    {
+        for (i = 0; i < bytes_read; i++)
+        {
+            switch (buffer[i])
+            {
+            case '\r':
+            case '\n':
+                exitcode = write(sfd, "\r\n", 2);
+                if_error(exitcode, "Unable to write to socket\n");
                 break;
-            case 'p':
-                port = atoi(optarg);
-                break;
-            case '?':
-                fprintf(stderr, "Unable to retrieve option\n");
-                exit(1);
             default:
-                fprintf(stderr, "Unknown option: %d\n", c);
-                exit(1);
+                exitcode = write(STDOUT_FILENO, &buffer[i], 1);
+                if_error(exitcode, "Unable to write to stdout\n");
+                break;
+            }
+        }
+    }
+    else
+    {
+        for (i = 0; i < bytes_read; i++)
+        {
+            switch (buffer[i])
+            {
+            case '\r':
+            case '\n':
+                exitcode = write(STDOUT_FILENO, "\r\n", 2);
+                if_error(exitcode, "Unable to write to stdout\n");
+                break;
+            default:
+                exitcode = write(STDOUT_FILENO, &buffer[i], 1);
+                if_error(exitcode, "Unable to write to stdout\n");
+                break;
+            }
         }
     }
 }
 
-void if_error(int error, char *message) {
-    if (error < 0) {
+void set_input_mode()
+{
+    struct termios newconfig;
+    // set input mode
+    exitcode = tcgetattr(STDIN_FILENO, &oldconfig);
+    if_error(exitcode, "Unable to get terminal parameters");
+    atexit(reset_input_mode);
+    exitcode = tcgetattr(STDIN_FILENO, &newconfig);
+    if_error(exitcode, "Unable to get terminal parameters");
+    newconfig.c_iflag = ISTRIP;
+    newconfig.c_oflag = 0;
+    newconfig.c_lflag = 0;
+    exitcode = tcsetattr(STDIN_FILENO, TCSANOW, &newconfig);
+    if_error(exitcode, "Unable to set terminal parameters");
+}
+
+void reset_input_mode()
+{
+    exitcode = tcsetattr(STDIN_FILENO, TCSANOW, &oldconfig);
+    if_error(exitcode, "Unable to set terminal parameters");
+}
+
+void parse_options(int argc, char **argv, int *port, char **log)
+{
+    int c;
+    static struct option long_options[] = {
+        {"port", required_argument, NULL, 'p'},
+        {"log", optional_argument, NULL, 'l'},
+        {"compress", no_argument, &compress_flag, 'c'},
+        {0, 0, 0, 0}};
+
+    while ((c = getopt_long(argc, argv, "plc", long_options, NULL)) != -1)
+    {
+        switch (c)
+        {
+        case 0:
+            // set flag
+            break;
+        case 'p':
+            *port = atoi(optarg);
+            break;
+        case 'l':
+            *log = optarg;
+            break;
+        case '?':
+            fprintf(stderr, "Unable to retrieve option\n");
+            exit(1);
+        default:
+            fprintf(stderr, "Unknown option: %d\n", c);
+            exit(1);
+        }
+    }
+}
+
+void if_error(int error, char *message)
+{
+    if (error < 0)
+    {
         fprintf(stderr, "%s\n", message);
         exit(1);
     }
--- p1b/lab1b-server.c
+++ p1b/lab1b-server.c
@@ -6,29 +6,34 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
-#define PORT 3000
+#include <poll.h>
+#include <signal.h>
+#include <sys/wait.h>
 
-static int port;
-int exitcode;
+static int compress_flag;
+int exitcode, sfd, newsfd, fwd[2], bwd[2];
 
-void parse_options(int argc, char **argv);
+void read_data(int fd, __pid_t pid);
+void parse_options(int argc, char **argv, int *port);
 void if_error(int error, char *message);
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    int port;
 
-    parse_options(argc, argv);
+    parse_options(argc, argv, &port);
 
-    int sfd, newsfd, bytes_read;
+    printf("PORT: %d\n", port);
+
     struct sockaddr_in addr;
     unsigned int len;
-    char buffer[1024];
 
     sfd = socket(AF_INET, SOCK_STREAM, 0);
     if_error(sfd, "Unable to open socket");
 
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(PORT);
+    addr.sin_port = htons(port);
     len = sizeof(addr);
 
     exitcode = bind(sfd, (struct sockaddr *)&addr, sizeof(addr));
@@ -37,49 +42,129 @@ int main(int argc, char **argv) {
     exitcode = listen(sfd, 5);
     if_error(exitcode, "Listen failed");
 
-    newsfd = accept(sfd, (struct sockaddr *)&addr, sizeof(addr));
+    newsfd = accept(sfd, (struct sockaddr *)&addr, (socklen_t *)&len);
     if_error(exitcode, "Accept failed");
 
-    bytes_read = read(newsfd, buffer, 1024);
-    if_error(bytes_read, "Unable to read from new socket");
+    exitcode = pipe(fwd);
+    if_error(exitcode, "Forward pipe failed");
+    exitcode = pipe(bwd);
+    if_error(exitcode, "Backward pipe failed");
 
-    printf("%s\n", buffer);
+    __pid_t pid = fork();
+    if_error(pid, "Fork failed");
+    if (pid > 0)
+    {
+        int i;
+        struct pollfd pfds[2];
+        exitcode = close(fwd[0]);
+        if_error(exitcode, "Unable to close read end of forward pipe");
+        exitcode = close(bwd[1]);
+        if_error(exitcode, "Unable to close write end of backward pipe");
 
-    char *test = "Server test";
-    send(newsfd, test, strlen(test), 0);
+        pfds[0].fd = newsfd;
+        pfds[1].fd = bwd[0];
+        pfds[0].events = POLLIN | POLLHUP | POLLERR;
+        pfds[1].events = POLLIN | POLLHUP | POLLERR;
 
-    printf("Server sent message successfully\n");
+        while (1)
+        {
+            exitcode = poll(pfds, 2, 0);
+            if_error(exitcode, "Poll failed");
+            for (i = 0; i < 2; i++)
+            {
+                if (pfds[i].revents & POLLIN)
+                    read_data(pfds[i].fd, pid);
+                if (pfds[i].revents & (POLLHUP | POLLERR))
+                    exit(0);
+            }
+        }
+    }
+    else
+    {
+        // child
+        exitcode = close(fwd[1]);
+        if_error(exitcode, "Unable to close write end of forward pipe");
+        exitcode = dup2(fwd[0], STDIN_FILENO);
+        if_error(exitcode, "Unable to dup forward read to stdin");
+        exitcode = dup2(bwd[1], STDOUT_FILENO);
+        if_error(exitcode, "Unable to dup backward write to stdout");
+        exitcode = dup2(bwd[1], STDERR_FILENO);
+        if_error(exitcode, "Unable to dup backward write to stderr");
+        exitcode = execl("/bin/bash", "bash", NULL);
+        if_error(exitcode, "Execl failed");
+    }
 
     exit(0);
 }
 
-void parse_options(int argc, char **argv) {
+void read_data(int fd, __pid_t pid)
+{
+    int i, bytes_read;
+    char buffer[256];
+    bytes_read = read(fd, buffer, 256);
+    if_error(bytes_read, "Unable to read buffer");
+    if (fd == newsfd)
+    {
+        for (i = 0; i < bytes_read; i++) {
+            switch (buffer[i]) {
+                case '\003':
+                    exitcode = kill(pid, SIGINT);
+                    if_error(exitcode, "Unable to kill child process");
+                    break;
+                case '\004':
+                    exitcode = close(fwd[1]);
+                    if_error(exitcode, "Unable to close write end of forward pipe");
+                    break;
+                case '\r':
+                case '\n':
+                    exitcode = write(fwd[1], "\n", 1);
+                    if_error(exitcode, "Unable to write forward pipe");
+                    break;
+                default:
+                    exitcode = write(fwd[1], &buffer[i], 1);
+                    if_error(exitcode, "Unable to write forward pipe");
+                    break;
+            }
+        }
+    }
+    else
+    {
+        exitcode = write(newsfd, &buffer, bytes_read);
+        if_error(bytes_read, "Unable to write to new socket file descriptor");
+    }
+}
+
+void parse_options(int argc, char **argv, int *port)
+{
     int c;
     static struct option long_options[] = {
         {"port", required_argument, NULL, 'p'},
-        {0, 0, 0, 0}
-    };
+        {0, 0, 0, 0}};
 
-    while ((c = getopt_long(argc, argv, "p", long_options, NULL)) != -1) {
-        switch (c) {
-            case 0:
-                // set flag
-                break;
-            case 'p':
-                port = atoi(optarg);
-                break;
-            case '?':
-                fprintf(stderr, "Unable to retrieve option\n");
-                exit(1);
-            default:
-                fprintf(stderr, "Unknown option: %d\n", c);
-                exit(1);
+    while ((c = getopt_long(argc, argv, "p", long_options, NULL)) != -1)
+    {
+        switch (c)
+        {
+        case 0:
+            // set flag
+            break;
+        case 'p':
+            *port = atoi(optarg);
+            break;
+        case '?':
+            fprintf(stderr, "Unable to retrieve option\n");
+            exit(1);
+        default:
+            fprintf(stderr, "Unknown option: %d\n", c);
+            exit(1);
         }
     }
 }
 
-void if_error(int error, char *message) {
-    if (error < 0) {
+void if_error(int error, char *message)
+{
+    if (error < 0)
+    {
         fprintf(stderr, "%s\n", message);
         exit(1);
     }
