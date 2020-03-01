e21a0babde142a93df84a807c173098ea908463e
Jiaping Zeng <jzeng@outlook.com>
2020-02-29 16:39:57 -0800

    implemented bfree and ifree

--- p3a/lab3a.c
+++ p3a/lab3a.c
@@ -6,14 +6,13 @@
 #include <sys/stat.h>
 #include "ext2_fs.h"
 
-int fd;
-const int block_size = 1024;
+int fd, block_size;
 struct ext2_super_block superblock;
 
 void print_superblock();
 void print_group();
-void print_bfree(u_int32_t position);
-void print_ifree();
+void print_bfree(u_int32_t pos);
+void print_ifree(u_int32_t pos);
 void print_inode();
 void print_dirent();
 void print_indirect();
@@ -39,7 +38,7 @@ int main(int argc, char **argv)
     // free block entries
     // print_bfree();
     // free I-node entries
-    print_ifree();
+    // print_ifree();
     // I-node summary
     print_inode();
     // directory entries
@@ -52,11 +51,12 @@ int main(int argc, char **argv)
 
 void print_superblock()
 {
-    pread(fd, &superblock, sizeof(superblock), block_size);
+    pread(fd, &superblock, sizeof(superblock), 1024);
+    block_size = EXT2_MIN_BLOCK_SIZE << superblock.s_log_block_size;
     printf("SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n",
            superblock.s_blocks_count,
            superblock.s_inodes_count,
-           EXT2_MIN_BLOCK_SIZE << superblock.s_log_block_size,
+           block_size,
            superblock.s_inode_size,
            superblock.s_blocks_per_group,
            superblock.s_inodes_per_group,
@@ -76,42 +76,45 @@ void print_group()
            group.bg_block_bitmap,
            group.bg_inode_bitmap,
            group.bg_inode_table);
-    print_bfree(1024 + sizeof(superblock) + sizeof(group));
+    print_bfree(group.bg_block_bitmap);
+    print_ifree(group.bg_inode_bitmap);
 }
 
-void print_bfree(u_int32_t position)
+void print_bfree(u_int32_t pos)
 {
-    char c, buffer[1024];
+    char c, buffer[8];
     u_int32_t i, j;
-    pread(fd, &buffer, block_size, position * block_size);
+    pread(fd, &buffer, superblock.s_blocks_count / 8, pos * block_size);
 
     for (i = 0; i < superblock.s_blocks_count / 8; i++)
     {
         c = buffer[i];
-        printf("byte: %d\n", c);
+        //printf("byte: %d\n", c);
         for (j = 0; j < 8; j++)
         {
             if (!(c & 1))
-                printf("BFREE,%d\n", i * 8 + j);
-            c >>= 2;
+                printf("BFREE,%d\n", i * 8 + j + 1);
+            c >>= 1;
         }
     }
-    /*
-    u_int32_t i, j;
-    char c;
-    for (i = 0; i < superblock.s_blocks_count / 8; i++)
-    {
-        pread(fd, &c, 1, position + i);
-        for (j = 0; j < 8; j++, c /= 2)
-            if (c % 2 == 0)
-                printf("BFREE,%d,%d\n", i * 8 + j, c);
-    }
-    */
 }
 
-void print_ifree()
+void print_ifree(u_int32_t pos)
 {
-    printf("IFREE\n");
+    char c, buffer[8];
+    u_int32_t i, j;
+    pread(fd, &buffer, superblock.s_inodes_count / 8, pos * block_size);
+    for (i = 0; i < superblock.s_inodes_count / 8; i++)
+    {
+        c = buffer[i];
+        //printf("byte: %d\n", c);
+        for (j = 0; j < 8; j++)
+        {
+            if (!(c & 1))
+                printf("IFREE,%d\n", i * 8 + j + 1);
+            c >>= 1;
+        }
+    }
 }
 
 void print_inode()
